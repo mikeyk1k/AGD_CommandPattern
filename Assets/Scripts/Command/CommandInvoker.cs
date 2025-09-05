f06c2901e0e9b4acf396a5703b5dea9b09ec1160
Mikey <mikeylearn430@gmail.com>
2025-09-05 18:05:11 +0700

    Add command namespace

---
+++ Assets/Scripts/Command/CommandInvoker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Command.Commands
+{
+    public class CommandInvoker
+    {
+        private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+
+        public void ProcessCommand(ICommand commandToProcess)
+        {
+            ExecuteCommand(commandToProcess);
+            RegisterCommand(commandToProcess);
+        }
+
+        public void ExecuteCommand(ICommand command) => command.Execute();
+        public void RegisterCommand(ICommand command) => commandRegistry.Push(command);
+    }
+}
--- Assets/Scripts/Commands/Abstract Commands/ICommand.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using UnityEngine;
-
-public interface ICommand
-{
-    // This method defines the contract for executing a command.
-    void Execute();
-}
