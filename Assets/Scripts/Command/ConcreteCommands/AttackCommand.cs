7eb829a9c8b0fb66c77ae4d4063dd242788867ca
Mikey <mikeylearn430@gmail.com>
2025-09-05 21:51:12 +0700

    Create Concrete Command and refactor action

---
+++ Assets/Scripts/Command/ConcreteCommands/AttackCommand.cs
@@ -0,0 +1,26 @@
+﻿using Command.Actions;
+using Command.Main;
+using System.Collections;
+using UnityEngine;
+
+namespace Command.Commands
+{
+    public class AttackCommand : UnitCommand
+    {
+        private bool willHitTarget;
+
+        public AttackCommand(CommandData commandData)
+        {
+            this.commandData = commandData;
+            willHitTarget = WillHitTarget();
+        }
+
+        public override void Execute()
+        {
+            IAction action = GameService.Instance.ActionService.GetActionByType(CommandType.Attack);
+            action.PerformAction(actorUnit, targetUnit, willHitTarget);
+        }
+
+        public override bool WillHitTarget() => true;
+    }
+}
